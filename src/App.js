c15a0d90d37349ea4a447d9d52ffff71bf06dfe9
Ridhwan Adagiri <ibrahimridwan47@gmail.com>
2020-02-08 15:46:23 -1100

    Implemented routing for the homepage and hatsPage

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import './App.css';
-import Homepage from "./Homepage.Component";
+import Homepage from "./pages/homepage/Homepage.Component";
+import { Route, Switch } from 'react-router-dom'
+
+const Hatspage = () => (
+  <div>
+  <h1>HatsPage</h1>
+  </div>
+)
 
 function App() {
   return (
     <div >
- <Homepage />
+    <Switch>
+    <Route exact path="/" component={Homepage} />
+    <Route path="/hats" component={Hatspage} />
+    </Switch>
     </div>
   );
 }
